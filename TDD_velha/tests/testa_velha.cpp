8e84995a475a4aa6fab79414ba1d55fb7ec1f985
RafaelCDA <rafaelcutrimalmeida@gmail.com>
2025-09-21 18:41:34 -0300

    chore: criado os testes diagonais, por linha e coluna para o player 1

--- TDD_velha/tests/testa_velha.cpp
+++ TDD_velha/tests/testa_velha.cpp
@@ -24,3 +24,13 @@ TEST_CASE("X vence por linha", "[linha]")
   };
   REQUIRE(VerificaVelha(t) == 1);
 }
+
+TEST_CASE("X vence por diagonal", "[diagonal]")
+{
+  int t[3][3] = {
+      {1, 2, 1},
+      {2, 1, 0},
+      {0, 2, 1},
+  };
+  REQUIRE(VerificaVelha(t) == 1);
+}
