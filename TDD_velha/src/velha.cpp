f25bec00acda6130f061e0214d2441ad3f401081
RafaelCDA <rafaelcutrimalmeida@gmail.com>
2025-09-21 18:18:07 -0300

    test: verde, verifica a vitória do jogador 1

--- TDD_velha/src/velha.cpp
+++ TDD_velha/src/velha.cpp
@@ -5,10 +5,32 @@
 
 #include "velha.hpp"
 
-int VerificaVelha(int velha[3][3])
+int VerificaVelha(int tab[3][3])
 {
-	// Implementação mínima para começar no "vermelho".
-	// Nos próximos ciclos, vamos implementar as regras e fazer os testes ficarem verdes.
-	(void)velha; // evita warning por parâmetro não usado
-	return 0;
+	// Colunas
+	for (int c = 0; c < 3; ++c)
+	{
+		if (tab[0][c] != 0 && tab[0][c] == tab[1][c] && tab[1][c] == tab[2][c])
+		{
+			return tab[0][c];
+		}
+	}
+	// Linhas
+	for (int r = 0; r < 3; ++r)
+	{
+		if (tab[r][0] != 0 && tab[r][0] == tab[r][1] && tab[r][1] == tab[r][2])
+		{
+			return tab[r][0];
+		}
+	}
+	// Diagonais
+	if (tab[0][0] != 0 && tab[0][0] == tab[1][1] && tab[1][1] == tab[2][2])
+	{
+		return tab[0][0];
+	}
+	if (tab[0][2] != 0 && tab[0][2] == tab[1][1] && tab[1][1] == tab[2][0])
+	{
+		return tab[0][2];
+	}
+	return 0; // ninguém venceu
 }
